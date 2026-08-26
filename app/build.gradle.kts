bf4834ae32821395f682239961af58c8fb79a2ec
RAFI <rmrashahriar@gmail.com>
2026-08-26 21:02:27 +0530

    Release v2.0.1 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.deanybytes.quranicwords"
         minSdk = 24
         targetSdk = 37
-        versionCode = 9
-        versionName = "2.0.0"
+        versionCode = 10
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
