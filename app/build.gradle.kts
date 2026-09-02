a0dda1930055abc6397ae7c6fd3d48753020c9b9
RAFI <rmrashahriar@gmail.com>
2026-09-02 10:21:03 +0530

    chore(release): bump version to 2.4.0 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.deanybytes.quranicwords"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.3.0"
+        versionCode = 14
+        versionName = "2.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
